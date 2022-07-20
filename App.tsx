b399c19061f03c708a161d8c95b99783bf6bb214
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2022-07-20 10:24:45 -0400

    Small change

--- App.tsx
+++ App.tsx
@@ -23,6 +23,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path='*' element={<Home />}></Route>
           </Routes>
       </div>
       
